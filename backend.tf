9d0ce64c6bc328e42e1ca5dd0125c9e66be348f2
Celine V <cyv419@gmail.com>
2024-03-14 17:13:07 -0500

    added tf code

--- backend.tf
+++ backend.tf
@@ -5,10 +5,10 @@
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
   cloud {
-    organization = "{{ORGANIZATION_NAME}}"
+    organization = "celinev-hashicorp"
 
     workspaces {
-      name = "{{WORKSPACE_NAME}}"
+      name = "tf-vcs-demo"
     }
   }
 
